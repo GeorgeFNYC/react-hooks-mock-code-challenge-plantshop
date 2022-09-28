fbeef2ee7ec3687e38571f1d8e64a7ca82f3f073
George Fernandez <fernandezg704@gmail.com>
2022-09-28 19:13:09 -0400

    commit tree c3bbc25e096a5b1d99fde9ccd169b04c7969ab67

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
-  return (
+
+  const [plantList, setPlantList] = useState([]);
+  const [search, setSearch] = useState("");
+
+
+  useEffect(() => {
+    fetch('http://localhost:6001/plants')
+    .then(res => res.json())
+    .then(plant => setPlantList(plant))
+  }, []);
+
+  const filtered = plantList.filter((plant) =>
+    plant.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm plantList = { plantList } setPlantList = { setPlantList }/>
+      <Search setSearch = { setSearch }/>
+      <PlantList plantList = { filtered }/>
     </main>
   );
 }
